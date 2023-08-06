f7a2fda30fab9c61099e732c1e15f9ace26d7b7f
Brian Etaghene <joveee05@gmail.com>
2023-08-06 14:32:13 +0100

    [Deleted] spec.ts

--- src/log-consumer/log-consumer.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { LogConsumerService } from './log-consumer.service';
-
-@Controller()
-export class LogConsumerController {
-  constructor(private readonly logConsumerService: LogConsumerService) {}
-}
